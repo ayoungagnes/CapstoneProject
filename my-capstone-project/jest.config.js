9810a52bb3ed49a0dab3e27bbb3fe14984342197
ayoungagnes <ayagnes07@gmail.com>
2025-07-20 17:40:50 +1000

    Fix set up for test

--- my-capstone-project/jest.config.js
+++ my-capstone-project/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   testEnvironment: "node",
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1", // 👈 matches jsconfig.json
-  },
   transform: {
-    "^.+\\.js$": "babel-jest",
+    "^.+\\.js$": ["babel-jest", { configFile: "./babel-jest.config.js" }],
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
 };
